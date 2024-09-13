7e89bbe365444f43c4ceff409c36dc929a48f015
SERPALETICIA <127907504+SERPALETICIA@users.noreply.github.com>
2024-09-12 22:00:24 -0300

    Aula 12-09

--- mater-play-frontend/src/App.tsx
+++ mater-play-frontend/src/App.tsx
@@ -1,14 +1,18 @@
+import Footer from "./app/components/Footer"
 import Header from "./app/components/Header"
 
 function App() {
 
   return (
     <div className="wrapper">
-      <Header></Header>
+      <Header />
+      <main></main>
+      <Footer />
+    
     </div>
 
 
   )
-}
+} 
 
 export default App
--- mater-play-frontend/src/app/components/Header/index.tsx
+++ mater-play-frontend/src/app/components/Header/index.tsx
@@ -1,21 +1,32 @@
-import './style.css';
+
+import { AppBar, Box, Button, Container, styled, Toolbar, Typography } from "@mui/material";
+
+const MenuButton = styled(Button)({
+    color:'white',
+    
+})
 
 function Header(){
     return (
-        <header>
-            <div className="container">
-                <div className="toolbar">
-                    <h6>Mater Play</h6>
-                    <nav>
-                        <ul>
-                            <li><a href="#">Todos</a></li>
-                            <li><a href="#">Series</a></li>
-                            <li><a href="#">Filmes</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
+        <AppBar>
+            <Container>
+                <Toolbar>
+                    <Typography variant="h6"
+                    >
+                        Mater Play
+                    </Typography>
+                    <Box
+                    sx={{
+                        paddingLeft: '1rem'
+                    }}
+                    >
+                        <MenuButton variant="text" disabled>Todos</MenuButton>
+                        <MenuButton variant="text">Series</MenuButton>
+                        <MenuButton variant="text">Todos</MenuButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
